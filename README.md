e3c37cb8a8c8b289e5de25f325a80c97a383829f
Tobias Schifftner <ts@ambimax.de>
2016-10-02 14:57:42 +0200

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# ambimax® QtySwitcher
+
+Qty Switcher for Magento 1 adds buttons to every qty input field for easier adjustments on tablets and smartphones
+
+![Example Screenshot](doc/ambimax-qtyswitcher-example.png "Example Screenshot")
+
+##  Installation
+
+For installation use composer and modman or copy files manually.
+
+### composer
+```
+"require": {
+    "ambimax/magento-module-ambimax-qtyswitcher": "~1.0"
+}
+```
+
+## Configuration
+
+Configuration is found in tab ```System > Configuration > Catalog > ambimax® Qty Switcher```
+
+## License
+
+[MIT License](http://choosealicense.com/licenses/mit/)
+
+## Author Information
+
+ - [Tobias Schifftner](https://twitter.com/tschifftner), [ambimax® GmbH](https://www.ambimax.de)
---
+++ src/js/ambimax/qtyswitcher/qtyswitcher.js
@@ -0,0 +1,83 @@
+/**
+ * Qty Switcher for Magento adds buttons to every qty input field for easier adjustments on tablets and smartphones
+ *
+ * @author      Tobias Schifftner <ts@ambimax.de>, ambimax® GmbH
+ * @copyright   2016 ambimax® GmbH
+ * @website     https://www.ambimax.de
+ * @licence     MIT
+ */
+qtyswitcher={
+    /**
+     * Init qty switcher
+     *
+     * @param _config
+     */
+    init:function(_config){
+
+        // set config
+        this.config = _config;
+
+        // add html
+        this.addQtyButtonsHtml();
+
+        // Add observer
+        $$('.qty-minus').each(function(element) {
+            element.observe('click', qtyswitcher.decreaseQty);
+        });
+        $$('.qty-plus').each(function(element) {
+            element.observe('click', qtyswitcher.increaseQty);
+        });
+    },
+    /**
+     * Add qty switcher html
+     */
+    addQtyButtonsHtml: function() {
+        $$('.qty').each(function(qtyElement){
+            qtyElement.insert({
+                before: '<button type="button" title="' + qtyswitcher.config.minusButtonLabel + '" class="button qty-minus" value="-"><span><span>-</span></span></button>',
+                after: '<button type="button" title="' + qtyswitcher.config.plusButtonLabel + '" class="button qty-plus" value="+"><span><span>+</span></span></button>'
+            });
+        });
+    },
+    /**
+     * Update qty form input
+     *
+     * @param qtyElement
+     * @param buttonElement
+     * @param qty
+     * @returns {qtyswitcher}
+     */
+    updateQty: function(qtyElement, buttonElement, qty) {
+        qtyElement.value = qty > 0 ? qty : 0;
+        // qtyElement.writeAttribute('value', qty);
+        return this;
+    },
+    /**
+     * Increase qty by 1
+     *
+     * @returns {qtyswitcher}
+     */
+    increaseQty: function () {
+        var qtyElement = $(this).previous('input.qty');
+
+        if(qtyElement) {
+            qtyswitcher.updateQty(qtyElement, $(this), ++qtyElement.value);
+        }
+
+        return this;
+    },
+    /**
+     * Decrease qty by 1
+     *
+     * @returns {qtyswitcher}
+     */
+    decreaseQty: function () {
+        var qtyElement = $(this).next('input.qty');
+
+        if(qtyElement) {
+            qtyswitcher.updateQty(qtyElement, $(this), --qtyElement.value);
+        }
+
+        return this;
+    }
+};
